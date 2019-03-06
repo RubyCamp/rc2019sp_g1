78d8df359857f91332ffee38300d5d2654ca4cc3
yoshi557 <44191269+yoshi557@users.noreply.github.com>
2019-03-06 15:45:22 +0900

    Add files via upload

--- Scenes/Option/director.rb
+++ Scenes/Option/director.rb
@@ -9,10 +9,10 @@ module Option
     end
 
     def initialize
-       @image =  Image.load('scenes/game/yellow.png')
+       @image =  Image.load('images/yellow.png')
        @font = Font.new(40)
        @font1 = Font.new(20)
-       @sound = Sound.new("scenes/game/button84.wav")
+       #@sound = Sound.new("scenes/game/button84.wav")
        @h1 = {:x => 50, :y => 200,:label => "速度:",:font => @font}
        @h2 = {:x => 50, :y => 300,:label => "物体の種類:",:font => @font}
        @h3 = {:x => 170, :y => 200,:label => "低速",:font => @font}
@@ -20,6 +20,8 @@ module Option
        @h5 = {:x => 370, :y => 200,:label => "高速",:font => @font}
        @h6 = {:x => 370, :y => 300,:label => "円",:font => @font}
        @h7 = {:x => 270, :y => 300,:label => "四角",:font => @font}
+       @shape = nil
+       @speed = nil
      end
 
      def play
@@ -27,51 +29,52 @@ module Option
        Window.draw(0, 0, @image)
        Window.draw_font(0, 0, "戻る", @font1, color: [255, 0, 0])
 
-
-
        [@h1,@h2,@h3,@h4,@h5,@h6,@h7].each do |hash|
            draw_option(hash[:x],hash[:y],hash[:label],hash[:font])
         end
         # x = 0
         #while x == 0
         #slow
-        x = 0
+        #x = 0
         if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 150 && Input.mouse_x <= 250 && Input.mouse_y >= 150 && Input.mouse_y <= 220
-          sound.play
-          x = 1
-          speed = :slow
+          #sound.play
+
+          @speed = :slow
         end
         #normal
         if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 251 && Input.mouse_x <= 320 && Input.mouse_y >= 150 && Input.mouse_y <= 220
-          sound.play
-          x = 1
-          speed = :normal
+          #sound.play
+
+          @speed = :normal
         end
         #fast
         if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 350 && Input.mouse_x <= 450 && Input.mouse_y >= 150 && Input.mouse_y <= 220
-          sound.play
-          x = 1
-          speed = :fast
+          #sound.play
+
+          @speed = :fast
+
         end
         #end
 
         #while x == 1
         #circle
         if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 350 && Input.mouse_x <= 420 && Input.mouse_y >= 280 && Input.mouse_y <= 350
-          sound.play
-          y = 1
-          shape = :circle
+          #sound.play
+
+          @shape = :circle
+
         end
         #square
         if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 250 && Input.mouse_x <= 320 && Input.mouse_y >= 280 && Input.mouse_y <= 350
-          sound.play
-          y = 1
-          shape = :square
-         end
-        #end
+          #sound.play
 
-        if x == 1 && y == 1
-          h[:speed],h[:shape] = speed, shape
+          @shape = :square
+
+        end        #end
+
+        if @speed && @shape
+
+          h[:speed],h[:shape] = @speed, @shape
           @@options = h
           scene_transition
         end
@@ -84,7 +87,7 @@ module Option
 
       def scene_transition
         Scene.move_to(:opening) if Input.mouse_push?(M_LBUTTON) && Input.mouse_x >= 0 && Input.mouse_x <= 20 && Input.mouse_x >= 0 && Input.mouse_x <= 20
-        Scene.move_to(:game, @@options) 
+        Scene.move_to(:game, @@options)
       end
   end
 end
